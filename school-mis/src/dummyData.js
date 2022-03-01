481a44962dd05726ab5d0650d8a966510bb75098
nivlapeter <nivlapeter.0549@gmail.com>
2022-03-01 11:37:27 +0300

    add dummy data and routes in app

---
+++ school-mis/src/dummyData.js
@@ -0,0 +1,108 @@
+export const studentRows = [
+  {
+    id: 1,
+    username: 'Festus Ribiro',
+    // avatar:
+    //   "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: 'jon@gmail.com',
+    status: 'active',
+    result: 'A',
+    role: 'Admin',
+  },
+  {
+    id: 2,
+    username: 'Salma Mohammed',
+    // avatar:
+    //   "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: 'jon@gmail.com',
+    status: 'active',
+    result: 'A',
+    role: 'Student',
+  },
+  {
+    id: 3,
+    username: 'Alphonse Mumo',
+    // avatar:
+    //   "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: 'jon@gmail.com',
+    status: 'active',
+    result: 'A',
+    role: 'Teacher',
+  },
+  {
+    id: 4,
+    username: 'John Mutua',
+    // avatar:
+    //   "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: 'jon@gmail.com',
+    status: 'active',
+    result: 'A',
+  },
+  {
+    id: 5,
+    username: 'Kennedy Thuo',
+    // avatar:
+    //   "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: 'jon@gmail.com',
+    status: 'active',
+    result: 'A',
+  },
+];
+
+export const reportsRows = [
+  {
+    id: 1,
+    username: 'FRibiro',
+    FullName: 'Festus Ribiro',
+    // avatar:
+    //   "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: 'fes@gmail.com',
+    status: 'active',
+    AggregateGrade: 'A',
+    attendance: '80%',
+  },
+  {
+    id: 2,
+    username: 'SalmaMo',
+    FullName: 'Salma Mohammed',
+    // avatar:
+    //   "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: 'salma@gmail.com',
+    status: 'active',
+    AggregateGrade: 'A',
+    attendance: '75%',
+  },
+  {
+    id: 3,
+    username: 'Alphmo',
+    FullName: 'Alphonse Mumo',
+    // avatar:
+    //   "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: 'alphonce@gmail.com',
+    status: 'active',
+    AggregateGrade: 'A',
+    attendance: '70%',
+  },
+  {
+    id: 4,
+    username: 'JohnMa',
+    FullName: 'John Mutua',
+    // avatar:
+    //   "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: 'john@gmail.com',
+    status: 'active',
+    AggregateGrade: 'A',
+    attendance: '100%',
+  },
+  {
+    id: 5,
+    username: 'KennThuo',
+    FullName: 'Kennedy Thuo',
+    // avatar:
+    //   "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: 'ken@gmail.com',
+    status: 'active',
+    AggregateGrade: 'A',
+    attendance: '95%',
+  },
+];
